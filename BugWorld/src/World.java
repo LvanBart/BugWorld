05342020f6269c655bac2227392401bd57249551
vanbarlaur <vanbarlaur@vanbarlaur-VirtualBox>
2016-07-26 15:39:06 +1200

    Added bugs to grid

--- BugWorld/src/World.java
+++ BugWorld/src/World.java
@@ -12,11 +12,11 @@ public class World {
 		this.height = 50;
 		this.width = 50;
 		
-		Bug bug1 = new Bug();
-		Bug bug2 = new Bug("cockroach", "Penelope", 'C', 150, 150, 23, 73645);
-		Bug bug3 = new JumpingBug();
-		Bug bug4 = new CrawlingBug();
-		Bug bug5 = new FlyingBug();
+		Bug bug1 = new Bug("fly", "James", 'B', 12, 10, 50, 7628368);
+		Bug bug2 = new Bug("cockroach", "Penelope", 'C', 20, 20, 23, 73645);
+		Bug bug3 = new JumpingBug("grasshopper", "Earl", 'J', 1, 1, 45, 2839847);
+		Bug bug4 = new CrawlingBug("beetle", "Patty", 'C', 8, 19, 45, 2839847);
+		Bug bug5 = new FlyingBug("bee", "Lola", 'F', 4, 7, 45, 2839847);
 		
 		bugs.add(bug1);
 		bugs.add(bug2);
@@ -44,6 +44,8 @@ public class World {
 		bugs.add(bug4);
 		bugs.add(bug5);
 		
+		this.drawWorld();
+		
 		
 	}
 	
@@ -56,11 +58,20 @@ public class World {
 		}
 		System.out.println("|");
 		
+		
 		// draw grid contents
 		for (int row = 1; row < width + 1; row++) {
 			System.out.print("|");
-			for (int col = 0; col < height; col++) {
-				System.out.print(" ");
+			for (int col = 1; col < height + 1; col++) {
+				String letter = " ";
+				for (Bug b: this.bugs) {
+					
+					if (b.getX() == col && b.getY() == row) {
+						letter = b.getSymbol() + "";
+					}	
+					
+				}
+				System.out.print(letter);
 			}
 			System.out.println("|");
 		}
